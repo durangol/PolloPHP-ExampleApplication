051990b5ff38f0b3a332d97c1358e10c63728b8f
njr1489 <njr1489@msn.com>
2013-02-09 19:34:25 -0500

    Added autoloader example

--- public/index.php
+++ public/index.php
@@ -13,7 +13,18 @@ define('LIBRARY_PATH', __DIR__ . '/../library');
 define('APPLICATION_PATH', __DIR__ . '/../application');
 define('APPLICATION_ENV', getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production');
 
+// Set include paths
+set_include_path(implode(PATH_SEPARATOR, array(
+    LIBRARY_PATH,
+    APPLICATION_PATH,
+    get_include_path()
+)));
+
 // Set Autoloader
+require LIBRARY_PATH . '/Pollo/Autoloader.php';
+require LIBRARY_PATH . '/Pollo/Autoloader/StandardAutoloader.php';
+$autoloader = new \Pollo\Autoloader();
+$autoloader->addAutoloader(new \Pollo\Autoloader\StandardAutoloader());
 
 // Set Application components
 $config = new \Pollo\Application\Config(APPLICATION_PATH . '/config/application.php');
