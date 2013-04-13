ee12701642b8f19da723fde927ac444f6e8ffce3
Luis M Durango <durangolm85@gmail.com>
2013-04-13 18:32:54 -0400

    setting up application with new code

--- public/index.php
+++ public/index.php
@@ -1,7 +1,8 @@
 <?php
+ini_set('html_errors', 1);
+
 /**
-* PolloPHP Application
-* 
+* PolloPHP Application* 
 * The entry point of the application
 * 
 * @link https://github.com/durangol/PolloPHP-ExampleApplication
@@ -9,8 +10,8 @@
 */
 
 // Set constants
-define('LIBRARY_PATH', __DIR__ . '/../library');
-define('APPLICATION_PATH', __DIR__ . '/../application');
+define('LIBRARY_PATH', realpath(__DIR__ . '/../library'));
+define('APPLICATION_PATH', realpath(__DIR__ . '/../application'));
 define('APPLICATION_ENV', getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production');
 
 // Set include paths
@@ -20,17 +21,19 @@ set_include_path(implode(PATH_SEPARATOR, array(
     get_include_path()
 )));
 
-// Set Autoloader
 require LIBRARY_PATH . '/Pollo/Autoloader.php';
 require LIBRARY_PATH . '/Pollo/Autoloader/StandardAutoloader.php';
 $autoloader = new \Pollo\Autoloader();
-$autoloader->addAutoloader(new \Pollo\Autoloader\StandardAutoloader());
+$autoloader->addAutoloader('Pollo', new \Pollo\Autoloader\StandardAutoloader(array(
+	'Pollo' => LIBRARY_PATH . '/Pollo'
+)));
 
 // Set Application components
-$config = new \Pollo\Application\Config(APPLICATION_PATH . '/config/application.php');
+$config = new \Pollo\Application\Config(require APPLICATION_PATH . '/config/application.php');
 $bootstrap = new \Pollo\Application\Bootstrap($config);
+$router = new \Pollo\Application\Mvc\Router(array());
 $dispatcher = new \Pollo\Application\Mvc\Dispatcher(new \Pollo\Request\Http(), new \Pollo\Response\Http());
 
 // Create and run Application
-$application = new \Pollo\Application($bootstrap, $dispatcher, APPLICATION_ENV);
+$application = new \Pollo\Application($bootstrap, $router, $dispatcher, APPLICATION_ENV);
 $application->run();
